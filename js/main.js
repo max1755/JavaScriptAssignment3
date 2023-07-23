a205258776eb05c29f06ff1fa2210a1b930bbba6
max1755 <112920950+max1755@users.noreply.github.com>
2023-07-22 23:23:50 -0400

    change to table

--- js/main.js
+++ js/main.js
@@ -1,27 +1,47 @@
 const input = document.getElementById("input");
 const submit = document.getElementById("submit");
-const list = document.querySelector("ul");
+const list = document.querySelector("table");
 
 submit.addEventListener("click", function(){
-    const listItem = document.createElement("li"); //the new item to add to the list of to-do's
 
-    const checkBox = document.createElement("INPUT"); //creating the checkbox for each item
+    const itemRow = document.createElement("tr"); //row for new item
+    const itemString = "" + input.value; //capturing input field text
+
+    if (!itemString) //if string is empty don't bother making cell, nothing entered
+    {
+
+    }else{
+
+    const td1 = document.createElement("td"); //cell for text
+    const td2 = document.createElement("td"); //cell for checkbox
+    const td3 = document.createElement("td"); //cell for delete button
+
+    //creating the checkbox for each item
+    const checkBox = document.createElement("INPUT"); 
     checkBox.setAttribute("type", "checkbox");
-
     checkBox.addEventListener("change", function(){
-        listItem.style.textDecoration === "line-through" ? listItem.style.textDecoration = "" 
-        : listItem.style.textDecoration = "line-through"; //ternary condition for checking un-checking items
+        td1.style.textDecoration === "line-through" ? td1.style.textDecoration = "" 
+        : td1.style.textDecoration = "line-through"; //ternary condition for checking un-checking items
     })
 
+    //Creating delete button
     const deleteMe = document.createElement("INPUT"); //creating a way to remove the item
     deleteMe.setAttribute("type", "button");
     deleteMe.setAttribute("value", "remove");
-
     deleteMe.addEventListener("click", function(){
-    list.removeChild(listItem);})
+    list.removeChild(itemRow);
+    })
 
-    listItem.textContent = input.value; //adding the new item to the list
-    listItem.appendChild(checkBox);
-    listItem.appendChild(deleteMe);
-    list.appendChild(listItem);
+    //Adding the new creation to the table
+    td1.textContent = itemString;
+    td2.appendChild(checkBox);
+    td3.appendChild(deleteMe);
+    
+    itemRow.appendChild(td1);
+    itemRow.appendChild(td2);
+    itemRow.appendChild(td3);
+    list.appendChild(itemRow);
+
+    input.value = "";
+    }
 })
